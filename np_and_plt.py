b3c07f924162e7c7e933e56ee5d3eabc506b1490
motsat <konpeiex@gmail.com>
2017-08-01 17:49:32 +0900

    cos and sin plt

--- np_and_plt.py
+++ np_and_plt.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.arange(0, 6, 0.1)
-y = np.sin(x)
-
-plt.plot(x, y)
+y1 = np.sin(x)
+y2 = np.cos(x)
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")
+plt.title('sin & cos')
+plt.legend()
 plt.show()
