328f3aab921ad2a164cef7afc540e9c88660857e
motsat <konpeiex@gmail.com>
2017-08-02 15:59:48 +0900

    network fix

--- network.py
+++ network.py
@@ -4,12 +4,23 @@ import matplotlib.pyplot as plt
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
-X = np.array([ 1.0, 0.5])
-W = np.array([ [0.1, 0.3, 0.5], [0.2, 0.4, 0.6] ])
-B = np.array([0.1, 0.2, 0.3])
+def identity_function(x):
+  return x
 
-A1 = np.dot(X,W) + B
+X = np.array([ 1.0, 0.5])
+W1 = np.array([ [0.1, 0.3, 0.5], [0.2, 0.4, 0.6] ])
+B1 = np.array([0.1, 0.2, 0.3])
+
+A1 = np.dot(X, W1) + B1
 print(A1)
 
 Z1 = sigmoid(A1)
 print(Z1)
+
+W2 = np.array([ [0.1, 0.4], [0.2, 0.5], [0.3, 0.6] ])
+B2 = np.array([0.1, 0.2])
+
+A2 = np.dot(Z1, W2) + B2
+print(A2)
+Z2 = identity_function(A2)
+print(Z2)
