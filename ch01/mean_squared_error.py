a6a5182c3cbef0ad94e77288dd3707627e560e98
motsat <konpeiex@gmail.com>
2017-08-05 23:59:35 +0900

    mean_squared_error fix

--- ch01/mean_squared_error.py
+++ ch01/mean_squared_error.py
@@ -5,6 +5,6 @@ def mean_squared_error(y, t):
     #data = 1e-7
     #return -np.sum(t = np.log(y + delta))
 
-t = [0, 0, 1, 0, 0, 0, 0,  0, 0, 0 ]
-y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0,  0., 0, 0 ]
-mean_squared_error(t, y)
+t = np.array([0, 0, 1, 0, 0, 0, 0,  0, 0, 0 ])
+y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0,  0., 0, 0 ])
+print(mean_squared_error(t, y))
