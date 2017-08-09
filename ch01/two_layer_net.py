7824f36262ec28e095bdee379750e64391c341cf
motsat <konpeiex@gmail.com>
2017-08-09 14:07:41 +0900

    update two layer net

--- ch01/two_layer_net.py
+++ ch01/two_layer_net.py
@@ -13,19 +13,17 @@ class TwoLayerNet:
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(hidden_size * output_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
     def predict(self, x):
         W1, W2 = self.params['W1'], self.params['W2']
         b1, b2 = self.params['b1'], self.params['b2']
 
-        # このあたりがなぜ?
         a1 = np.dot(x, W1) + b1
         z1 = sigmoid(a1)
         a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-        return y
+        return softmax(a2)
     def loss(self, x, t):
         return cross_entropy_error(self.predict(x), t)
     def accuracy(self, x, t):
@@ -38,9 +36,26 @@ class TwoLayerNet:
     def numerical_gradient(self, x, t):
         loss_W = lambda W: self.loss(x, t)
         grads = {}
-        grad['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
         grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
         grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
         grads['b2'] = numerical_gradient(loss_W, self.params['b1'])
         return grads
 
+net = TwoLayerNet(input_size = 784, hidden_size = 100, output_size = 10)
+print(net.params['W1'].shape)
+print(net.params['b1'].shape)
+print(net.params['W2'].shape)
+print(net.params['b2'].shape)
+
+x = np.random.rand(100, 784)
+y = net.predict(x)
+
+x = np.random.rand(100, 784)
+t = np.random.rand(100, 10)
+
+grads = net.numerical_gradient(x, t) 
+print(grads['W1'].shape)
+print(grads['b1'].shape)
+print(grads['W2'].shape)
+print(grads['b2'].shape)
