b4bc95620fc698756055f888085773cab17c28c0
motsat <konpeiex@gmail.com>
2017-08-15 05:59:06 +0900

    gradient_descent fix

--- ch01/gradient_descent.py
+++ ch01/gradient_descent.py
@@ -8,7 +8,6 @@ def numerical_gradient(f, x):
         x[idx] = tmp_val + h
         fxh1 = f(x)
 
-        tmp_val = x[idx]
         x[idx] = tmp_val - h
         fxh2 = f(x)
 
@@ -18,9 +17,7 @@ def numerical_gradient(f, x):
 
 def gradient_descent(f, init_x, lr=0.01, step_num=100):
     x = init_x
-    print(x)
-    print(lr)
-    print(step_num)
+
     for i in range(step_num):
         grad = numerical_gradient(f, x)
         x -= lr * grad
@@ -28,7 +25,7 @@ def gradient_descent(f, init_x, lr=0.01, step_num=100):
     return x
 
 def function_2(x):
-    return x[0] ** 2 + x[1] ** 2
+    return x[0]**2 + x[1]**2
 
 init_x = np.array([-3.0, 4.0])
-print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
+print(gradient_descent(function_2, init_x, lr=0.1, step_num=100))
